23e9d4d1cb819d8e054d1f2082eff95c7c03d53e
dev <dev@qiyutech.tech>
2021-01-12 10:35:24 +0800

    use set -e for simplify shell

--- supervisor/entrypoint.sh
+++ supervisor/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# 初始化超级管理员
-(cd $SUPERVISOR_DJANGO_DIRECTORY && python manage.py migrate) || {
-        echo '数据库迁移失败' ;
-        exit 1;
-}
+set -e
 
-# 启动 supervisor 监控
+echo "Django 开始 migrate 数据库..."
+cd $SUPERVISOR_DJANGO_DIRECTORY && python manage.py migrate
+echo "数据库迁移成功"
+
+echo "启动 supervisord 管理主进程"
 /usr/local/bin/supervisord -c /app/supervisor/daemon.ini
