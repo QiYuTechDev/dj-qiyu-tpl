e377d40ffba5371fe8344dccb8536dee73db1e7e
dev <dev@qiyutech.tech>
2021-03-01 11:45:32 +0800

    only allow once

--- dj_qiyu_tpl/static/dj_qiyu_tpl/js/app_doc_node.js
+++ dj_qiyu_tpl/static/dj_qiyu_tpl/js/app_doc_node.js
@@ -1,43 +1,47 @@
 // prevent load multi times
 (function () {
-    if (typeof AppDocNode !== "undefined") {
-        return;
-    }
-
-    class AppDocNode extends HTMLDivElement {
-        constructor() {
-            super();
-
-            const shadow = this.attachShadow({mode: 'closed'});
-
-            shadow.innerHTML = this.innerHTML;
-            // remove first empty node [indent failed eg]
-            //
-            //   import os
-            // import sys
-            //
-            shadow.querySelectorAll("code").forEach(function (code) {
-                let first = code.firstChild;
-                if (first === null) {
-                    return;
-                }
-                if (first.nodeName === "#text" && first.textContent.trim() === "") {
-                    code.removeChild(first);
-                }
-            });
-
-            [
-                "/static/dj_qiyu_tpl/vendor/rst/minimal.css",
-                "/static/dj_qiyu_tpl/vendor/rst/plain.css",
-            ].forEach(function (href) {
-                let style = document.createElement("link");
-                style.rel = "stylesheet";
-                style.type = "text/css";
-                style.href = href;
-                shadow.appendChild(style);
-            });
+    let executed = false;
+    (function () {
+        if (executed) {
+            return;
         }
-    }
+        executed = true;
 
-    customElements.define("app-doc", AppDocNode, {extends: "div"});
+        class AppDocNode extends HTMLDivElement {
+            constructor() {
+                super();
+
+                const shadow = this.attachShadow({mode: 'closed'});
+
+                shadow.innerHTML = this.innerHTML;
+                // remove first empty node [indent failed eg]
+                //
+                //   import os
+                // import sys
+                //
+                shadow.querySelectorAll("code").forEach(function (code) {
+                    let first = code.firstChild;
+                    if (first === null) {
+                        return;
+                    }
+                    if (first.nodeName === "#text" && first.textContent.trim() === "") {
+                        code.removeChild(first);
+                    }
+                });
+
+                [
+                    "/static/dj_qiyu_tpl/vendor/rst/minimal.css",
+                    "/static/dj_qiyu_tpl/vendor/rst/plain.css",
+                ].forEach(function (href) {
+                    let style = document.createElement("link");
+                    style.rel = "stylesheet";
+                    style.type = "text/css";
+                    style.href = href;
+                    shadow.appendChild(style);
+                });
+            }
+        }
+
+        customElements.define("app-doc", AppDocNode, {extends: "div"});
+    })();
 })();
