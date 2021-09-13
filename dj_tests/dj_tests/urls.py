b822c4d04c4c4f962564448b27d1b368e8df3000
奇遇科技 <69790869+QiYuTechDev@users.noreply.github.com>
2021-09-13 13:46:18 +0000

    合并拉取请求 #4
    * add pagination v2

--- dj_tests/dj_tests/urls.py
+++ dj_tests/dj_tests/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("demo", views.demo_view),
     path("app_doc", views.app_doc_tag_view),
     path("rst_doc", views.rst_doc_tag_view),
-    path("pc_mobile", views.pc_mobile_render)
+    path("pc_mobile", views.pc_mobile_render),
+    path("pagination", views.PaginationView.as_view()),
 ]
--- dj_tests/tpl_test/test_pagination_render.py
+++ dj_tests/tpl_test/test_pagination_render.py
@@ -1,17 +1,19 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.test import Client
 from django.test import TestCase
 
 
-class AppDocTagTestCase(TestCase):
+class AppPaginationRender(TestCase):
     def setUp(self) -> None:
         self._http = Client()
 
-    def test_app_doc_tag(self):
-        resp = self._http.get("/app_doc")
+    def test_pagination_render(self):
+        User.objects.create_user('hello')
+        User.objects.create_user('world')
+
+        resp = self._http.get("/pagination")
         assert isinstance(resp, HttpResponse)
         assert resp.status_code == 200
         content = resp.content.decode()
-        assert "app-doc" in content
-        assert "hello world" in content
-        assert "this is a demo" in content
+        print(content)
