db9a507f25541ec3e84965725551925a5a4c70e3
奇遇科技 <69790869+QiYuTechDev@users.noreply.github.com>
2021-12-29 11:04:32 +0800

    Year 2022 (#9)
    * bump year to 2022

--- dj_qiyu_tpl/templates/dj_qiyu_tpl/footer.pyi
+++ dj_qiyu_tpl/templates/dj_qiyu_tpl/footer.pyi
@@ -1,3 +1,9 @@
+# This file is type hint for Django Template
+#
+# You need use this PyCharm plugin for proper work:
+# https://plugins.jetbrains.com/plugin/18232-typed-django-template
+#
+
 from typing import Optional
 
 icp: Optional[str]  # ICP 备案号
